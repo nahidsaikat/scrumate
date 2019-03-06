52fe2dfd484db6a517bc9c7117d667e4f89a2b64
nahidsaikat <nahidsaikatft40@gmail.com>
2019-03-06 14:02:49 +0600

    <15> Adding core ui dashboard template.

--- scrumate/settings.py
+++ scrumate/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'scrumate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'core/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "core/static")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
--- scrumate/urls.py
+++ scrumate/urls.py
@@ -14,8 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    # path('', include('core.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', views.serve),
+        re_path(r'^(?P<path>.*)$', views.serve),
+    ]
+
