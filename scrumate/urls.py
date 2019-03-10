f85ea39c478a28b777178e53195e320b96565001
nahidsaikat <nahidsaikatft40@gmail.com>
2019-03-10 12:32:20 +0600

    <19> login_required decorator in home url.

--- scrumate/urls.py
+++ scrumate/urls.py
@@ -19,11 +19,15 @@ from django.views.generic.base import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles import views
+from django.contrib.auth.decorators import login_required
+from core.views import index
+
 
 urlpatterns = [
+    path('', index, name='index'),
+    path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     # path('', include('core.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
