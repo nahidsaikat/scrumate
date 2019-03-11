2a8bc3432c9438873c2470e39dfa20625762603d
nahidsaikat <nahidsaikatft40@gmail.com>
2019-03-11 23:48:35 +0600

    <27> Pagination in project list page.

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Project
 
 
 @login_required(login_url='/login/')
@@ -9,4 +11,15 @@ def index(request, **kwargs):
 
 @login_required(login_url='/login/')
 def project_list(request, **kwargs):
-    return render(request, 'projects/project_list.html')
+    project_list = Project.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(project_list, 10)
+    try:
+        projects = paginator.page(page)
+    except PageNotAnInteger:
+        projects = paginator.page(1)
+    except EmptyPage:
+        projects = paginator.page(paginator.num_pages)
+
+    return render(request, 'projects/project_list.html', {'projects': projects})
