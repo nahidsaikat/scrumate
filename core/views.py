f5dd3c9ef89b61450b0aa25f1285c04b6ac9b810
nahidsaikat <nahidsaikatft40@gmail.com>
2019-03-12 20:20:16 +0600

    <28> Applying filters in project list page.

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.conf import settings
 from .models import Project
+from .filters import ProjectFilter
 
 
 @login_required(login_url='/login/')
@@ -11,10 +13,11 @@ def index(request, **kwargs):
 
 @login_required(login_url='/login/')
 def project_list(request, **kwargs):
-    project_list = Project.objects.all()
+    project_filter = ProjectFilter(request.GET, queryset=Project.objects.all())
+    project_list = project_filter.qs
     page = request.GET.get('page', 1)
 
-    paginator = Paginator(project_list, 10)
+    paginator = Paginator(project_list, settings.PAGE_SIZE)
     try:
         projects = paginator.page(page)
     except PageNotAnInteger:
@@ -22,4 +25,4 @@ def project_list(request, **kwargs):
     except EmptyPage:
         projects = paginator.page(paginator.num_pages)
 
-    return render(request, 'projects/project_list.html', {'projects': projects})
+    return render(request, 'projects/project_list.html', {'projects': projects, 'filter': project_filter})
