c716c3f5a78b3a6fcf42dad6d2e2c754627fe11d
nahidsaikat <nahidsaikatft40@gmail.com>
2019-05-11 00:34:59 +0600

    <177> Moving index_*.html files to general folder

--- scrumate/general/views.py
+++ scrumate/general/views.py
@@ -5,23 +5,23 @@ from django.shortcuts import render
 @login_required(login_url='/login/')
 def settings(request, **kwargs):
     employee = request.user.employee if request.user and hasattr(request.user, 'employee') else None
-    return render(request, 'index_settings.html', {'employee': employee})
+    return render(request, 'general/index_settings.html', {'employee': employee})
 
 
 @login_required(login_url='/login/')
 def reports(request, **kwargs):
     employee = request.user.employee if request.user and hasattr(request.user, 'employee') else None
-    return render(request, 'index_reports.html', {'employee': employee})
+    return render(request, 'general/index_reports.html', {'employee': employee})
 
 
 @login_required(login_url='/login/')
 def sprint(request, **kwargs):
     employee = request.user.employee if request.user and hasattr(request.user, 'employee') else None
-    return render(request, 'index_sprint.html', {'employee': employee})
+    return render(request, 'general/index_sprint.html', {'employee': employee})
 
 
 @login_required(login_url='/login/')
 def project(request, **kwargs):
     employee = request.user.employee if request.user and hasattr(request.user, 'employee') else None
-    return render(request, 'index_project.html', {'employee': employee})
+    return render(request, 'general/index_project.html', {'employee': employee})
 
