71886c75fb590d8fbcbd3b9e09921389d1edef3c
nahidsaikat <nahidsaikatft40@gmail.com>
2019-05-11 01:05:04 +0600

    <178> Dashboard for sprint

--- scrumate/general/views.py
+++ scrumate/general/views.py
@@ -1,5 +1,10 @@
+from _datetime import datetime
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+from scrumate.core.choices import DeliverableStatus
+from scrumate.core.models import Deliverable
 
 
 @login_required(login_url='/login/')
@@ -16,8 +21,19 @@ def reports(request, **kwargs):
 
 @login_required(login_url='/login/')
 def sprint(request, **kwargs):
-    employee = request.user.employee if request.user and hasattr(request.user, 'employee') else None
-    return render(request, 'general/index_sprint.html', {'employee': employee})
+    today = datetime.today()
+    deliverable_qs = Deliverable.objects.filter(sprint__start_date__lte=today, sprint__end_date__gte=today)
+    pending = deliverable_qs.filter(status=DeliverableStatus.Pending)
+    in_progress = deliverable_qs.filter(status=DeliverableStatus.InProgress)
+    done = deliverable_qs.filter(status=DeliverableStatus.Done)
+
+    data = {
+        'pending': pending,
+        'in_progress': in_progress,
+        'done': done,
+    }
+
+    return render(request, 'general/index_sprint.html', {'data': data})
 
 
 @login_required(login_url='/login/')
