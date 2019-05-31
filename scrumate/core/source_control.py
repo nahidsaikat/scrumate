d0fe8570af6d9be4af1ab24694981b9cc6cf143f
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-01 00:06:12 +0600

    <221> Add ProjectCommitLog model with some refactoring in source control system

--- scrumate/core/source_control.py
+++ scrumate/core/source_control.py
@@ -1,9 +1,9 @@
 from github import Github
+from github.GithubObject import NotSet
 
 
-def get_commit_messages(project):
-    commit_messages = []
+def get_commit_messages(project, since=None):
     github = Github(project.git_username, project.git_password)
-    for commit in github.get_user().get_repo(project.git_repo).get_commits():
-        commit_messages.append(commit.commit.message)
-    return commit_messages
+    if not since:
+        since = NotSet
+    return github.get_user().get_repo(project.git_repo).get_commits(since=since)
