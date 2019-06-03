42aeacf0da210f6b15baa950c96fa7ee501a3712
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-03 23:55:44 +0600

    <251> delete choices.py file from core folder

--- scrumate/core/choices.py
+++
@@ -1,8 +0,0 @@
-from djchoices import DjangoChoices, ChoiceItem
-
-
-class OverTimeStatus(DjangoChoices):
-    Pending = ChoiceItem(1, 'Pending')
-    Acknowledged = ChoiceItem(2, 'Acknowledged')
-    Done = ChoiceItem(3, 'Done')
-    Rejected = ChoiceItem(4, 'Rejected')
