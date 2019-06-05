15dc43974d79de98a39b830aa72479215f98829a
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-05 11:57:21 +0600

    <279> Move release related model to release folder

---
+++ scrumate/core/release/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from scrumate.core.project.models import Project
+
+User = get_user_model()
+
+
+class Release(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.TextField(default='', null=True, blank=True)
+    version = models.CharField(max_length=100, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    release_date = models.DateField(default=None)
+    delivery_date = models.DateField(default=None, null=True, blank=True)
+    release_log = models.TextField(default=None, null=True, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, default=None, null=True, blank=True, related_name='created_releases')
+    approved_by = models.ForeignKey(User, on_delete=models.SET_NULL, default=None, null=True, blank=True, related_name='approved_releases')
+    comment = models.TextField(default='', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
