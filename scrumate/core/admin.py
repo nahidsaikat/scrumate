8019248d3a0d137ffd958d14bfd8a90f548774e2
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-22 00:13:51 +0600

    <296> Registering model with simple history admin

--- scrumate/core/admin.py
+++ scrumate/core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 
 from scrumate.core.deliverable.models import Deliverable
 from scrumate.core.project.models import ProjectCommitLog, Project
@@ -7,10 +8,10 @@ from scrumate.core.sprint.models import Sprint
 from scrumate.core.task.models import Task
 from scrumate.core.user_story.models import UserStory
 
-admin.site.register(Project)
-admin.site.register(ProjectCommitLog)
-admin.site.register(Release)
-admin.site.register(UserStory)
+admin.site.register(Project, SimpleHistoryAdmin)
+admin.site.register(ProjectCommitLog, SimpleHistoryAdmin)
+admin.site.register(Release, SimpleHistoryAdmin)
+admin.site.register(UserStory, SimpleHistoryAdmin)
 # admin.site.register(Division)
 # admin.site.register(Dashboard)
 # admin.site.register(Portlet)
@@ -18,9 +19,9 @@ admin.site.register(UserStory)
 # admin.site.register(Priority)
 # admin.site.register(Label)
 # admin.site.register(Issue)
-admin.site.register(Sprint)
-admin.site.register(Task)
-admin.site.register(Deliverable)
+admin.site.register(Sprint, SimpleHistoryAdmin)
+admin.site.register(Task, SimpleHistoryAdmin)
+admin.site.register(Deliverable, SimpleHistoryAdmin)
 # admin.site.register(DailyScrum)
 # admin.site.register(OverTime)
 # admin.site.register(Party)
--- scrumate/people/admin.py
+++ scrumate/people/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+
 from scrumate.people.models import Department, Designation, Employee, Client
 
 
-admin.site.register(Department)
-admin.site.register(Designation)
-admin.site.register(Employee)
-admin.site.register(Client)
+admin.site.register(Department, SimpleHistoryAdmin)
+admin.site.register(Designation, SimpleHistoryAdmin)
+admin.site.register(Employee, SimpleHistoryAdmin)
+admin.site.register(Client, SimpleHistoryAdmin)
