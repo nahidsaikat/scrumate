42a945b5430c16fc83d124cb48539853e76ba986
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-23 02:39:41 +0600

    <311> History page for release

--- scrumate/core/release/urls.py
+++ scrumate/core/release/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.release_list, name='release_list'),
     path('add/', views.release_add, name='release_add'),
     path('<int:pk>/edit/', views.release_edit, name='release_edit'),
+    path('<int:pk>/history/', views.ReleaseHistoryList.as_view(), name='release_history'),
 ]
--- scrumate/core/release/views.py
+++ scrumate/core/release/views.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 
 from scrumate.general.decorators import project_owner
 from scrumate.core.project.models import Project
 from scrumate.core.release.models import Release
 from scrumate.core.release.filters import ReleaseFilter
 from scrumate.core.release.forms import ReleaseForm
+from scrumate.general.views import HistoryList
 
 
 @login_required(login_url='/login/')
@@ -57,3 +59,28 @@ def release_edit(request, project_id, pk, **kwargs):
     title = 'Edit Release'
     project = Project.objects.get(pk=project_id)
     return render(request, 'core/common_add.html', {'form': form, 'title': title, 'list_url_name': 'release_list', 'project': project})
+
+
+class ReleaseHistoryList(HistoryList):
+    permission_required = 'scrumate.core.release_history'
+
+    def get_release_id(self):
+        return self.kwargs.get('pk')
+
+    def get_project_id(self):
+        return self.kwargs.get('project_id')
+
+    def get_queryset(self):
+        return Release.history.filter(id=self.get_release_id())
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        project = Project.objects.get(pk=self.get_project_id())
+        release = Release.objects.get(pk=self.get_release_id())
+
+        context['project'] = project
+        context['title'] = f'History of {release.name}'
+        context['back_url'] = reverse('release_list', kwargs={'project_id': self.get_project_id()})
+        context['base_template'] = 'general/index_project_view.html'
+        return context
+
