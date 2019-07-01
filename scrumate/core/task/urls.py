3a0850caa993108e2221b4de6071227e419b9a1e
nahidsaikat <nahidsaikatft40@gmail.com>
2019-07-02 01:46:04 +0600

    Task view page link added in list page

--- scrumate/core/task/urls.py
+++ scrumate/core/task/urls.py
@@ -5,6 +5,7 @@ from scrumate.core.task import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add/', views.task_add, name='task_add'),
+    path('<int:pk>/', views.TaskDetailView.as_view(), name='task_view'),
     path('<int:pk>/edit/', views.task_edit, name='task_edit'),
     path('<int:pk>/update_status/', views.update_task_status, name='update_task_status'),
     path('<int:pk>/history/', views.TaskHistoryList.as_view(), name='task_history'),
--- scrumate/core/task/views.py
+++ scrumate/core/task/views.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.generic import DetailView
 
 from scrumate.core.project.models import Project
 from scrumate.core.task.models import Task
@@ -108,4 +109,21 @@ class TaskHistoryList(HistoryList):
         context['title'] = f'History of {task.name}'
         context['back_url'] = reverse('task_list', kwargs={'project_id': self.get_project_id()})
         context['base_template'] = 'general/index_project_view.html'
+        return context
+
+
+class TaskDetailView(DetailView):
+    queryset = Task.objects.all()
+    template_name = 'includes/generic_view.html'
+    context_object_name = 'task'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        project_id = self.kwargs.get('project_id')
+        instance = self.get_object()
+        context['form'] = TaskForm(instance=instance)
+        context['edit_url'] = reverse('task_edit', kwargs={'project_id': project_id, 'pk': instance.pk})
+        context['list_url'] = reverse('task_list', kwargs={'project_id': project_id})
+        context['title'] = instance.name
+        context['project'] = Project.objects.get(pk=project_id)
         return context
